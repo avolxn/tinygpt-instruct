f2f222c16f7ac0fe5d90befa15556ca2ba5f8597
avolxn <vlad.eldashov2303@mail.ru>
2025-12-30 03:01:40 +0500

    base tokenizer interface

---
+++ src/tinygpt_instruct/tokenizer.py
@@ -0,0 +1,18 @@
+class Tokenizer:
+    def __init__(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
+
+    def train(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
